277adeaea7f94b3d36eaa8046fffadec995e3623
Borbély Gergő Árpád <borbelygergo06@gmail.com>
2023-03-24 09:16:03 +0100

    A feladat megoldva

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
+const doc = {
+  megmunkaltHossz: document.querySelector("#megmunkaltHossz"),
+  fordulatonkentiElotolas: document.querySelector("#fordulatonkentiElotolas"),
+  foorsoFordulatszam: document.querySelector("#foorsoFordulatszam"),
+  megmunkalasiIdo: document.querySelector("#megmunkalasiIdo"),
+};
+
 function startCalc() {
-  calcResult();
+  let hossz = Number(doc.megmunkaltHossz.value);
+  let elotolas = Number(doc.fordulatonkentiElotolas.value);
+  let fordulatszam = Number(doc.foorsoFordulatszam.value);
+  doc.megmunkalasiIdo.value = calcResult(hossz, elotolas, fordulatszam);
 }
 
-function calcResult() {}
+function calcResult(hossz, elotolas, fordulatszam) {
+    return hossz / (elotolas * fordulatszam);
+}
